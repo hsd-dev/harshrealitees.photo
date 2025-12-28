a5f43ac0a8b7b1416979f9dccfcb8dae6f485fd4
Harsh Deshpande <harsh.deshpande@neura-robotics.com>
2025-12-28 18:57:27 +0100

    fix: add snipcart store

---
+++ src/data/store.js
@@ -0,0 +1,76 @@
+export const storeProducts = [
+  {
+    id: 'print-urban-01',
+    name: 'Urban Streets Print #01',
+    description: 'High-quality print of urban street photography',
+    price: 49.99,
+    image: 'https://64.media.tumblr.com/d7ff5d99377afd1bb2f199e7c56f4429/567c37d23c29cb6c-7a/s500x750/c624918751137c253f30219c978f81d3215d177d.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  },
+  {
+    id: 'print-urban-02',
+    name: 'Urban Streets Print #02',
+    description: 'Street photography capturing city life moments',
+    price: 49.99,
+    image: 'https://64.media.tumblr.com/760f400607773107b99a5c947adb8de0/567c37d23c29cb6c-a1/s500x750/3a1e804e8b5a341955ed6d81e04193827cacb880.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  },
+  {
+    id: 'print-urban-03',
+    name: 'Urban Streets Print #03',
+    description: 'Urban landscape photography print',
+    price: 49.99,
+    image: 'https://64.media.tumblr.com/ce73ba0c58310f1426293f41d69d77a6/567c37d23c29cb6c-76/s500x750/a32a0f1435f95b22f2570b1f395250007daec8a9.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  },
+  {
+    id: 'print-nature-01',
+    name: 'Nature Landscape Print #01',
+    description: 'Beautiful nature landscape print',
+    price: 59.99,
+    image: 'https://64.media.tumblr.com/8584fee992a5b57ee9120a34572bc1c6/567c37d23c29cb6c-6d/s500x750/4d9f57dcc3de86b4e79a4eefb739bdcb74d48484.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  },
+  {
+    id: 'print-nature-02',
+    name: 'Nature Landscape Print #02',
+    description: 'Scenic natural beauty captured in print',
+    price: 59.99,
+    image: 'https://64.media.tumblr.com/7a37992e9daff951701c45843c1343ff/567c37d23c29cb6c-79/s500x750/6e437ec137fd9fee9753eff3876192fd67aa8f0d.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  },
+  {
+    id: 'print-nature-03',
+    name: 'Nature Landscape Print #03',
+    description: 'Stunning landscape photography print',
+    price: 59.99,
+    image: 'https://64.media.tumblr.com/f357c63befacba72e62f925164e05b4a/567c37d23c29cb6c-42/s500x750/241c3cd6e93705b831508c53cc8413373d0924df.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  },
+  {
+    id: 'print-portrait-01',
+    name: 'Portrait Print #01',
+    description: 'Character study portrait print',
+    price: 54.99,
+    image: 'https://64.media.tumblr.com/c226f10ed9a7cb497945eece6a75a6d1/567c37d23c29cb6c-2e/s500x750/d99c6d7580cb6d5801eb455f7f3b4eb4f688b8c6.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  },
+  {
+    id: 'print-portrait-02',
+    name: 'Portrait Print #02',
+    description: 'People photography portrait print',
+    price: 54.99,
+    image: 'https://64.media.tumblr.com/13d115340d585e45672ecbb2c7b355d5/567c37d23c29cb6c-4b/s500x750/9a2c310663b297304ed466b1d7497e20e38e4479.jpg',
+    category: 'Prints',
+    sizes: ['8x10', '11x14', '16x20']
+  }
+];
+
+
